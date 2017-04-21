6c7d1ad62bfbcb3c703c28275a9316e395017e6d
邓琦 <dqdanavera@gmail.com>
2017-04-21 17:18:35 +0800

    更新 viewholder

--- app/src/main/java/com/dq/android/travelcarrytreasure/ui/discover/DiscoverViewHolder.java
+++ app/src/main/java/com/dq/android/travelcarrytreasure/ui/discover/DiscoverViewHolder.java
@@ -4,10 +4,9 @@ import android.content.Context;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
 import com.dq.android.travelcarrytreasure.R;
-import com.dq.android.travelcarrytreasure.model.baidulvyou.DiscoverResponse;
+import com.dq.android.travelcarrytreasure.model.baidulvyou.TravellerNoteResponse;
 import com.dq.android.travelcarrytreasure.util.TimeUtils;
 import support.ui.adapters.EasyViewHolder;
 
@@ -15,7 +14,7 @@ import support.ui.adapters.EasyViewHolder;
  * Created by DQDana on 2017/4/14
  */
 
-public class DiscoverViewHolder extends EasyViewHolder<DiscoverResponse.DataBean.ModListBean.ListBean> {
+public class DiscoverViewHolder extends EasyViewHolder<TravellerNoteResponse.DataBean.ListBean> {
 
   private final ImageView mImg;
   private TextView mTvTitle;
@@ -30,13 +29,14 @@ public class DiscoverViewHolder extends EasyViewHolder<DiscoverResponse.DataBean
     mTvTime = (TextView) itemView.findViewById(R.id.tv_time);
   }
 
-  @Override public void bindTo(int position, DiscoverResponse.DataBean.ModListBean.ListBean values) {
+  @Override public void bindTo(int position, TravellerNoteResponse.DataBean.ListBean values) {
     Glide.with(itemView.getContext())
         .load(values.getExt().getPic_url())
         .thumbnail(0.1f)
         .into(mImg);
     mTvTitle.setText(values.getExt().getTitle());
     mTvLookNum.setText(String.valueOf(values.getExt().getView_count()));
-    mTvTime.setText(TimeUtils.getDateFromTime(Long.parseLong(values.getExt().getStart_time()), "yyyy-MM-dd"));
+    mTvTime.setText(
+        TimeUtils.getDateFromTime(Long.parseLong(values.getExt().getStart_time()), "yyyy-MM-dd"));
   }
 }
