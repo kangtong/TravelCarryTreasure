62c57ecf2f34397da7923738d082163dd7fd6948
康桐 <kangtong1993@outlook.com>
2017-05-08 23:51:46 +0800

    链接高德 SDK

--- app/src/main/java/com/dq/android/travelcarrytreasure/ui/map/BaiDuMapActivity.java
+++ app/src/main/java/com/dq/android/travelcarrytreasure/ui/map/BaiDuMapActivity.java
@@ -2,20 +2,42 @@ package com.dq.android.travelcarrytreasure.ui.map;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.baidu.mapapi.SDKInitializer;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 import com.dq.android.travelcarrytreasure.R;
 
 public class BaiDuMapActivity extends AppCompatActivity {
-
+  MapView mMapView = null;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_baidu_map);
-
-    init();
+    mMapView = (MapView) findViewById(R.id.map);
+    mMapView.onCreate(savedInstanceState);// 此方法必须重写
+    AMap aMap = mMapView.getMap();
   }
-
-  private void init() {
-    SDKInitializer.initialize(this);
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+    mMapView.onDestroy();
+  }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+    mMapView.onResume();
+  }
+  @Override
+  protected void onPause() {
+    super.onPause();
+    //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+    mMapView.onPause();
+  }
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+    mMapView.onSaveInstanceState(outState);
   }
 }
--- app/src/main/java/com/dq/android/travelcarrytreasure/ui/mine/MineFragment.java
+++ app/src/main/java/com/dq/android/travelcarrytreasure/ui/mine/MineFragment.java
@@ -1,15 +1,25 @@
 package com.dq.android.travelcarrytreasure.ui.mine;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import com.dq.android.travelcarrytreasure.R;
 import com.dq.android.travelcarrytreasure.base.BaseFragment;
+import com.dq.android.travelcarrytreasure.ui.map.BaiDuMapActivity;
 
 /**
  * Created by DQDana on 2017/4/5
  */
 
 public class MineFragment extends BaseFragment {
+
+  Unbinder unbinder;
 
   public static MineFragment newInstance() {
     MineFragment fragment = new MineFragment();
@@ -25,4 +35,18 @@ public class MineFragment extends BaseFragment {
   @Override protected void initView(View view, Bundle savedInstanceState) {
 
   }
+
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // TODO: inflate a fragment view
+    View rootView = super.onCreateView(inflater, container, savedInstanceState);
+    Button button=(Button) rootView.findViewById(R.id.btn_map_test);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        getActivity().startActivity(new Intent(getActivity(), BaiDuMapActivity.class));
+      }
+    });
+    return rootView;
+  }
+
 }
